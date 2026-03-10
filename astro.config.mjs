417ac9a70d19a5076ff741b2888d045ba0315d7c
Ariiellus <arielcardenas95@hotmail.com>
2026-03-09 22:34:06 -0600

    chore: improve website tracing

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,9 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   output: 'static',
+  site: 'https://ariiellus.eth.limo',
+  trailingSlash: 'always',
+  build: {
+    format: 'directory',
+  },
 });
